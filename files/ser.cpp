a2953e61b88b6f637f729a31386a490508aeb14c
Vikash Murugesan <95967494+Vmos21@users.noreply.github.com>
2025-10-30 12:34:55 +0000

    Done :)

--- files/ser.cpp
+++ files/ser.cpp
@@ -1,19 +1,55 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 #define WIDTH 40
 
+class research
+{
+    public:
+        int constRes = 0;
+        int milRes = 0;
+        int ecoRes = 0;
+        int dipRes = 0;
 
-class services
+        research ()
+        {
+            return;
+        }
+
+        void doRes()
+        {
+            cout << setw(WIDTH) << setfill('-') << "" << endl;
+            cout << setw((WIDTH+8)/2) << setfill(' ') << "RESEARCH" << endl;
+            cout << setw(WIDTH) << setfill('-') << "" << endl;
+
+            int opt;
+
+            cout << "what would you like to build ?" << endl;
+            cout << "1. Infrastructure" << "\n2. Military" << "\n3. Economy" << "\n4. Diplomatics" << "\n5. Exit" << endl;
+            cout << ">>";
+            cin >> opt;
+        }
+        void showRes()
+        {
+            return;
+        }
+};
+
+
+class services : public research
 {
     public:
         int eduCenter = 0;
         int hos = 0;
         int publService = 0;
         int jobOpps = 0;
+
+        int constuctTime = 1/(1+this->constRes)*120;
 
         services ()
         {
@@ -28,14 +64,21 @@ class services
 
             int opt;
 
-            cout << "what would you like to build ?" << endl;
+            cout << "what would you like to build ? (Takes " << this->constuctTime << " secs)" << endl;
             cout << "1. Education Center" << "\n2. Hospital" << "\n3. Public Services" << "\n4. Jobs" << "\n5. Exit" << endl;
             cout << ">>";
             cin >> opt;
 
+            if (opt == 5)
+            {
+                return;
+            }
+            
+            this_thread::sleep_for(chrono::seconds(constuctTime));
+
             switch (opt)
             {
-                case 1: this->eduCenter++; cout << "Education Center Has been built." << endl;
+                case 1: eduCenter++; cout << "Education Center Has been built." << endl;
                     break;
 
                 case 2: this->hos++; cout << "Hospital has been built." << endl;
@@ -45,9 +88,6 @@ class services
                     break;
 
                 case 4: this->jobOpps++; cout << "More Jobs are created." << endl;
-                    break;
-
-                case 5: 
                     break;
 
                 default: cout << "No Command assigned." << endl;
@@ -69,34 +109,13 @@ class services
         }
 };
 
-class research
-{
-    public:
-        int milRes = 0;
-        int ecoRes = 0;
-        int dipRes = 0;
-
-        research ()
-        {
-            return;
-        }
-
-        void doRes()
-        {
-            return;
-        }
-        void showRes()
-        {
-            return;
-        }
-};
-
-
 int main()
 {
     services s = services();
 
     s.createSer();
     s.showSer();
+
+    s.doRes();
     return 0;
 }
