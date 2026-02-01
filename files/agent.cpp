8da4ed5fbab584b7d5d60bdc56a0a0270a27ed4c
Vikash Murugesan <95967494+Vmos21@users.noreply.github.com>
2026-02-01 08:54:21 +0000

    Agent Preping

---
+++ files/agent.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+
+
+int main()
+{
+
+    return 0;
+}
+
+
+// The agent is supposed to act as a buffer between Manip and whatever the file be.
+// Based on the file and the return types we gotta connect the ends.
